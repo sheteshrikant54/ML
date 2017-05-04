37a486af98e61f99dcb9868acfc243ab1d3edef6
Shrikant Shete <sheteshrikant54@users.noreply.github.com>
2017-05-03 21:55:06 -0400

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 import sklearn
 from sklearn import linear_model
 
-
 a56_train_filepath = 'algebra_2005_2006_train_sample.txt'
 
 a56data = pd.read_table(a56_train_filepath)
-#a56data = pd.read_csv("trainValid.csv", header=0)
+
 a56data.head()
 hierarchy = a56data['Problem Hierarchy']
 units, sections = [], []
@@ -16,11 +15,9 @@ for i in range(len(hierarchy)):
     units.append(hierarchy[i].split(',')[0].strip())
     sections.append(hierarchy[i].split(',')[1].strip())
 
-# Now add 'Units' and 'Sections' as columns within the dataframe
 a56data['Problem Unit'] = pd.Series(units, index=a56data.index)
 a56data['Problem Section'] = pd.Series(sections, index=a56data.index)
 
-# Rearrange order of columns
 cols = a56data.columns.tolist()
 cols = cols[0:3]+cols[-2::]+cols[3:-2]
 a56data = a56data[cols]
@@ -32,11 +29,9 @@ print 'There are {0} students, so we will be adding as many columns to this data
 
 numrows = len(df)
 
-# Add a column for every student and fill them with zeros
 for stud in students:
     df[stud] = pd.Series(np.zeros(numrows), index=df.index)
     
-# For each student's problem entries in the dataframe, mark their column as 1
 for stud in students:
     df.loc[df['Anon Student Id'] == stud,stud] = 1
 
@@ -47,11 +42,9 @@ print 'There are {0} unique problem units, so we will be adding as many columns 
 
 numrows = len(df)
 
-# Add a column for every problem unit and fill them with zeros
 for u in units:
     df[u] = pd.Series(np.zeros(numrows), index=df.index)
     
-# For each student's attempt at a problem, mark the appropriate problem unit as 1
 for u in units:
     df.loc[df['Problem Unit'] == u,u] = 1
 
@@ -62,87 +55,63 @@ print 'There are {0} unique problem sections, so we will be adding as many colum
 
 numrows = len(df)
 
-# Add a column for every problem unit and fill them with zeros
 for s in sections:
     df[s] = pd.Series(np.zeros(numrows), index=df.index)
     
-# For each student's attempt at a problem, mark the appropriate problem unit as 1
 for s in sections:
     df.loc[df['Problem Section'] == s,s] = 1
 
 print "Shape after adding Problem Section columns", np.shape(df)
 
-
 pnames = set(a56data['Problem Name'])
 print 'There are {0} unique problem names, so we will be adding as many columns to this dataframe.'.format(len(pnames))
 
-# Add a column for every problem unit and fill them with zeros
 for n in pnames:
     df[n] = pd.Series(np.zeros(len(df)), index=df.index)
     
-# For each student's attempt at a problem, mark the appropriate problem unit as 1
 for n in pnames:
     df.loc[df['Problem Name'] == n,n] = 1
 
 print "Shape after adding Problem Name columns", np.shape(df)
-
 
 snames = set(a56data['Step Name'])
 print 'There are {0} unique step names, so we will be adding as many columns to this dataframe.'.format(len(snames))
 
 numrows = len(df)
 
-# Add a column for every problem unit and fill them with zeros
 for n in snames:
     df[n] = pd.Series(np.zeros(numrows), index=df.index)
     
-# For each student's attempt at a problem, mark the appropriate problem unit as 1
 for n in snames:
     df.loc[df['Step Name'] == n,n] = 1
 
 print "Shape after adding Step Name columns", np.shape(df)
 
-# Create an empty testing dataframe
 testdf = pd.DataFrame(columns=df.columns)
 unique_units = list(units)
-# Create the testing set
 for i in range(len(unique_units)):
-    # Get the last problem of the current problem unit
     lastProb = list(df[df['Problem Unit'] == unique_units[i]]['Problem Name'])[-1]
     
-    # Get all the rows corresponding to the last problem for the given problem unit
     lastProbRows = a56data[(df['Problem Unit'] == unique_units[i]) & (df['Problem Name']==lastProb)]
-    
-    # Concatenate test dataframe with the rows just found
     testdf = pd.concat([testdf,lastProbRows])
 
-# Create a training dataframe that is equal to original dataframe with all the test cases removed
 trainIndex = list(set(df.index) - set(testdf.index))
 traindf = df.loc[trainIndex]
 
-# Get the target feature within the test set: the Correct First Attmpt
 CFAs = np.array(testdf['Correct First Attempt'])
 
-# Define a helper function for calculating the root-mean-square error
 def RMSE(p,y):
-    ''' The Root-Mean-Square Error takes the predicted values p for the target
-    variable y and takes the square root of the mean of the square of their
-    differences. '''
     return np.sqrt(np.sum(np.square(p-y))/len(y))
 
-# Test the RMSE for an array of all zeros
 p = np.zeros(len(CFAs))
-print 'An array of all zeros gives an RMSE of:',RMSE(p,CFAs)
+print 'An array of all 0 gives an RMSE of:',RMSE(p,CFAs)
 
-# Test the RMSE for an array of all ones
 p = np.ones(len(CFAs))
-print 'An array of all ones gives an RMSE of:',RMSE(p,CFAs)
+print 'An array of all 1 gives an RMSE of:',RMSE(p,CFAs)
 
-# Test the RMSE for an array of random 0s and 1s
 p = np.random.randint(0,2,len(CFAs)).astype(float)
-print 'An array of random ones and zeros gives an RMSE of:',RMSE(p,CFAs)
+print 'An array of random 1 and 0 gives an RMSE of:',RMSE(p,CFAs)
 
-# Define the logistic function
 def logisfunc(x):
     return 1.0 / (1.0 + np.exp(-x))
 
@@ -152,11 +121,9 @@ def logit_plots(X,y,model,feat_names):
     print num_samples, 'number of samples'
     print num_features, 'number of features'
     
-    # The coefficients and bias of the decision function
     coefs = model.coef_.ravel()
     bias  = model.intercept_
     
-    # Plot the decision function separately for each feature
     x_plt = np.linspace(-1.0,1.0,300)
     
     fig = plt.figure(figsize=(3*num_features,6))
@@ -173,14 +140,9 @@ def logit_plots(X,y,model,feat_names):
     plt.tight_layout()
 
 def error_metrics(p,yy):
-    '''Calculates the error metrics, i.e. the precision and recall.
-    Precision = True positives / Predicted positives
-    Recall    = True positives / Actual positives'''
     predicted_positives = len(p[p==1])
     actual_positives    = len(yy[yy==1])
-    # The predicted values for when actual values are 1
     pp = p[yy==1]
-    # True positives are when these predicted values are also 1
     true_positives      = len(pp[pp==1])
     false_positives     = len(yy) - true_positives
     
@@ -199,13 +161,8 @@ def error_metrics(p,yy):
     
     print '\nF_1 Score: ', F_1score
 
-# Define a helper function to normalize the feature matrix X
 import numba
 def autonorm(X):
-    ''' Calculates the mean and range of values of each column
-    in the matrix (features) subtracts the mean from each value
-    and divides by the range, thereby normalizing all values to
-    fall between -1 and 1.'''
     x_means = np.mean(X,axis=0)
     x_means = np.ones(np.shape(X))*x_means
     x_maxs  = np.max(X,axis=0)
@@ -215,7 +172,6 @@ def autonorm(X):
     return X_normd
 
 autonorm_jit = numba.jit(autonorm)
-
 
 features_to_norm = ['Step Duration (sec)','Hints','Problem View']
 binary_features = list(students)+list(units)+list(sections)+list(pnames)#+list(snames)
@@ -235,7 +191,6 @@ XX_to_norm = np.array(XX[features_to_norm])
 XX_nonnorm = np.array(XX[binary_features])
 XX_to_norm = autonorm(XX_to_norm)
 XX = np.concatenate((XX_to_norm,XX_nonnorm), axis=1)
-
 
 model = linear_model.LogisticRegression()
 
