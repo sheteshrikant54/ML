19783083324f8d2c40c365bf789260e52f938f72
Shrikant Shete <sheteshrikant54@users.noreply.github.com>
2017-05-03 20:48:05 -0400

    Update RandomForest_DecisionTree.py

--- RandomForest_DecisionTree.py
+++ RandomForest_DecisionTree.py
@@ -9,7 +9,6 @@ from sklearn.ensemble import RandomForestClassifier
 
 a56_train_filepath =  'algebra_2005_2006_train_160k.txt'
 a56data = pd.read_table(a56_train_filepath)
-#a56data = pd.read_csv("trainValid.csv", header=0)
 print a56data.head()
 
 hierarchy = a56data['Problem Hierarchy']
@@ -72,45 +71,33 @@ for idx,stn in enumerate(stns):
     stn_dict[idx] = stn
     df.loc[df[cat] == stn,cat] = idx
 print 'updated step nm'
-# Create an empty testing dataframe
+
 testdf = pd.DataFrame(columns=df.columns)
 
-# Create the testing set
 unique_units = list(set(df['Problem Unit']))
 for i in range(len(unique_units)):
-    # Get the last problem of the current problem unit
     lastProb = list(df[df['Problem Unit'] == unique_units[i]]['Problem Name'])[-1]
-    
-    # Get all the rows corresponding to the last problem for the given problem unit
     lastProbRows = a56data[(df['Problem Unit'] == unique_units[i]) & (df['Problem Name']==lastProb)]
-    
-    # Concatenate test dataframe with the rows just found
     testdf = pd.concat([testdf,lastProbRows])
 
-# Create a training dataframe that is equal to original dataframe with all the test cases removed
 trainIndex = list(set(df.index) - set(testdf.index))
 traindf = df.loc[trainIndex]
 print 'test data created'
 
-# Get the target feature within the test set: the Correct First Attmpt
 CFAs = np.array(testdf['Correct First Attempt'])
 
-# Define a helper function for calculating the root-mean-square error
 def RMSE(p,y):
     ''' The Root-Mean-Square Error takes the predicted values p for the target
     variable y and takes the square root of the mean of the square of their
     differences. '''
     return np.sqrt(np.sum(np.square(p-y))/len(y))
 
-# Test the RMSE for an array of all zeros
 p = np.zeros(len(CFAs))
 print 'An array of all zeros gives an RMSE of:',RMSE(p,CFAs)
 
-# Test the RMSE for an array of all ones
 p = np.ones(len(CFAs))
 print 'An array of all ones gives an RMSE of:',RMSE(p,CFAs)
 
-# Test the RMSE for an array of random 0s and 1s
 p = np.random.randint(0,2,len(CFAs)).astype(float)
 print 'An array of random ones and zeros gives an RMSE of:',RMSE(p,CFAs)
 
@@ -141,13 +128,7 @@ def error_metrics(p,yy):
     
     print '\nF_1 Score: ', F_1score
 
-
-# Define a helper function to normalize the feature matrix X
 def autonorm(X):
-    ''' Calculates the mean and range of values of each column
-    in the matrix (features) subtracts the mean from each value
-    and divides by the range, thereby normalizing all values to
-    fall between -1 and 1.'''
     x_means = np.mean(X,axis=0)
     x_means = np.ones(np.shape(X))*x_means
     x_maxs  = np.max(X,axis=0)
@@ -157,7 +138,6 @@ def autonorm(X):
     return X_normd
 
 autonorm_jit = numba.jit(autonorm)
-
 
 features_to_norm = ['Step Duration (sec)','Hints','Problem View']
 category_features = ['Anon Student Id', 'Problem Hierarchy', 'Problem Unit', 'Problem Section', 'Problem Name']
@@ -199,7 +179,6 @@ n_feats = len(features)
 feat_std = np.std([tree.feature_importances_ for tree in model.estimators_],axis=0)
 indices = np.argsort(importances)[::-1]
 
-# Print the feature ranking
 print("\nFeature ranking:")
 
 for f in range(n_feats):
